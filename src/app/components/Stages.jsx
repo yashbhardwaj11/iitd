65054b91d89ddffa08b3204d8da46f2630ed1dd6
saksham-guptaa <creator.sakshamgupta@gmail.com>
2024-09-26 15:48:15 +0530

    bug fix 7

--- src/app/components/Stages.jsx
+++ src/app/components/Stages.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 
 // Define the dynamic content array
 const mentorshipStages = [
