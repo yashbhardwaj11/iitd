5788ac37332430ef2002bcab0f8f49b7a2662a40
saksham-guptaa <creator.sakshamgupta@gmail.com>
2024-09-29 17:40:24 +0530

    Image fix

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Slider from "react-slick";
-import Image from "next/image";
 import { motion } from "framer-motion";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
